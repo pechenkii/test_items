7cca800e330638bb0137ff182e053912819e59be
pechenkii <83368958+pechenkii@users.noreply.github.com>
2021-05-27 18:40:01 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,13 +2,13 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def is_element_present(browser):
-    try:
-	browser.get(link)
-        browser.implicitly_wait(5)
-        browser.find_element_by_css_selector('.btn-add-to-basket')
-        return True
-    except:
-        return False 
+	try:
+		browser.get(link)
+		browser.implicitly_wait(5)
+		browser.find_element_by_css_selector('.btn-add-to-basket')
+		return True
+	except:
+		return False 
 def test_guest_should_see_add_basket_button(browser):
 	browser.get(link) 
 	assert is_element_present(browser)==True, "Button is not found"
